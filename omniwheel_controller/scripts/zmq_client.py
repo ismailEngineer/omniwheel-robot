edcb837958ecda3320f630963a0455dd1c73a5fe
Ismail <ismail_hamrouni@yahoo.fr>
2024-02-02 17:50:33 +0100

    Json message via zmq

--- omniwheel_controller/scripts/zmq_client.py
+++ omniwheel_controller/scripts/zmq_client.py
@@ -16,9 +16,10 @@ socket.connect("tcp://localhost:5555")
 
 #  Do 10 requests, waiting each time for a response
 for request in range(10):
-    print("Sending request %s …" % request)
-    socket.send(b"Hello")
+    request = {"ID": 123, "name": 321}
+    print("Sending request  …", request)
+    socket.send_json(request)
 
     #  Get the reply.
-    message = socket.recv()
-    print("Received reply %s [ %s ]" % (request, message))
+    message = socket.recv_json()
+    print("Received reply -->",message)
--- omniwheel_controller/scripts/zmq_server.py
+++ omniwheel_controller/scripts/zmq_server.py
@@ -7,11 +7,14 @@ socket.bind("tcp://*:5555")
 
 while True:
     #  Wait for next request from client
-    message = socket.recv()
-    print("Received request: %s" % message)
+    message = socket.recv_json()
+    print("Received request: ",message)
 
     #  Do some 'work'
     time.sleep(1)
 
+    # JSON MESSAGE 
+    ret = {"a": 123, "b": 321}
+
     #  Send reply back to client
-    socket.send(b"World")
+    socket.send_json(ret)
