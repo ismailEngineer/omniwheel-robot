545a911d694264c8265cbef05bcec537cc126260
Ismail <ismail_hamrouni@yahoo.fr>
2024-02-03 01:03:46 +0100

    Connect qt windows with raspberry via zmq

--- omniwheel_controller/scripts/gui_controller.py
+++ omniwheel_controller/scripts/gui_controller.py
@@ -14,8 +14,27 @@ class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('../ui/controller.ui', self)
+        self.on_off_button.clicked.connect(self.on_off_cliked)
+        self.m1_input_box.valueChanged.connect(self.show_result)
+        self.m2_input_box.valueChanged.connect(self.show_result)
+        self.m3_input_box.valueChanged.connect(self.show_result)
         self.show()
+
+    def on_off_cliked(self):
+        print("ON/OFF CLICKED !!")
+
+    def show_result(self):
+        global socket
+        print("IN ")
+        m1 = self.m1_input_box.value()
+        m2 = self.m2_input_box.value()
+        m3 = self.m3_input_box.value()
+        msg = init_json_message()
+        msg = edit_json_message(m1,m2,m3,5,6,msg)
+        send_json_msg(socket,msg) #bug of sending twice when using arrows of spinboxs
+        print("OUT ")
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
 app.exec_()
+
